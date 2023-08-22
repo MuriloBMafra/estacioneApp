5d4847312e039aa6ff7e7a97931491a6c1a4370a
MuriloMafra <murilommafra@gmail.com>
2023-08-22 16:26:54 -0400

    Initial commit

---
+++ lib/utils/constants.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+class Constants { //altera as cores primarias e secundarias do app
+  static final Color primaryColor = Colors.blue;
+  static final Color scaffoldBackgroundColor = Color.fromRGBO(245, 247, 249, 1);
+}
